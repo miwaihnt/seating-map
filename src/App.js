943c80ef04d8fb1cbfdb24395814c55136df344b
{miwa} <{miwa19970514@gmail.com}>
2023-08-20 13:42:48 +0900

    Mapcontainer-finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-function App() {
+const App = () => {
+  
+  const position = [51.505, -0.09]
+  const MapContainerStyle = {
+    width: "800px",
+    height: "600px"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={MapContainerStyle}>
+
+          <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br/> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>  
+    </>
+    );
 }
 
 export default App;
